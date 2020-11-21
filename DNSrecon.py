b160ae1199c54c8890830373a346a706c9ca4c1a
justasimplehacker <dan.kmm2003@gmail.com>
2020-11-21 23:36:55 +0630

    Beautified
    Just finalized the design

--- DNSrecon.py
+++ DNSrecon.py
@@ -1,16 +1,26 @@
 #!python3 
-#Written by NMM
+#Written by NMM 
 
-import json, requests, sys, csv
+import json, requests, sys, csv, time, colorama
+from colorama import Fore, Back, Style
 
 # Compute DomainName from command line arguments.
 if len(sys.argv) < 2:
-    print('Usage: DNSrecon.py <Domain_Name>\n')
+    print('Usage: DNSrecon.py .<Domain_Name>\n')
     sys.exit()
 domain = ' '.join(sys.argv[1:])
 
+
+
 #The url for searching dns data set
 url ='https://dns.bufferover.run/dns?q=%s' % (domain)
+print(" ")
+print(Fore.LIGHTBLUE_EX + "######################################")
+print(Fore.LIGHTBLUE_EX + "# Developed by NMM, Enhanced by KMM  #")
+print(Fore.LIGHTBLUE_EX + "######################################\r\n")
+print(Fore.CYAN + "[*] Getting You Subdomains Please Wait..\r\n")
+
+time.sleep(1)
 
 response = requests.get(url)
 response.raise_for_status()
@@ -36,7 +46,12 @@ else:
 
 		for IP_addr, Name in FDNS_Dictionary.items():
 			#print (f"The domain name \"{Name}\" has the IP address of \"{IP_addr}\"")
-			print(f"{Name} => {IP_addr}")
+			print(Fore.GREEN + f"{Name}" + Fore.WHITE + " => " + Fore.RED + f" {IP_addr}\r")
 			resultWriter.writerow((Name,IP_addr))
 
 SaveAsCsv.close()
+
+time.sleep(0.5)
+print(Fore.RESET + " ")
+print(Fore.LIGHTMAGENTA_EX + "Enjoy! our MF Collab @ github.com/naymyatmin & github.com/justasimplehacker \r\n")
+print(Style.RESET_ALL)
